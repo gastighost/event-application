66de88679e66703b5d50d7f277dfc7f802380419
Gaston Roxas <gacaro51597@gmail.com>
2022-04-13 15:56:35 +0800

    use static generation to get events

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import styles from "../styles/Home.module.css";
-import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-function Home() {
-  const featuredEvents = getFeaturedEvents();
-
+function Home(props) {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
 
 export default Home;
